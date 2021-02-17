c12c075951b99d1e7f40357f6e6a497781b5290b
Kris Foster <me@krismade.me>
2021-02-17 11:47:03 +0000

    Latest broken update

--- compile-only.sh
+++ compile-only.sh
@@ -26,31 +26,32 @@ echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
 
 native-image \
   --verbose \
-  --allow-incomplete-classpath \
   --no-fallback \
   --no-server \
   --install-exit-handlers \
   --enable-all-security-services \
   -Dspring.xml.ignore=false \
+  -H:GenerateDebugInfo=1 \
   -H:Name=${ARTIFACT} \
   -H:+PrintClassInitialization \
   -Dorg.apache.cxf.JDKBugHacks.all=true \
+  -Dspring.native.mode=agent \
+  -H:ConfigurationFileDirectories=./src/main/resources/META-INF/native-image \
+  --initialize-at-build-time=com.sun.xml.bind.v2.runtime.reflect.opt.Injector,com.sun.xml.bind.v2.schemagen.xmlschema \
   --initialize-at-run-time=org.hibernate.validator.internal.engine.messageinterpolation.el.SimpleELContext \
   -H:+ReportExceptionStackTraces \
   -cp ${CP} \
   ${MAINCLASS}
 
+#   --allow-incomplete-classpath \
 #  -Dorg.apache.cxf.jmx.disabled=true \
 #  -Dcxf.metrics.enabled=false \
 #  -Dmanagement.metrics.binders.jvm.enabled=false \
-
-
-#     -Dcxf.metrics.enabled=false \
-#     --initialize-at-run-time=org.hibernate.validator.internal.engine.messageinterpolation.el.SimpleELContext \
-#     -H:TraceClassInitialization=true \
-# -Dspring.native.mode=agent
-#    -Dspring.native.remove-xml-support=false \
-#    -Dspring.spel.ignore=false \
-#    -Dspring.native.remove-yaml-support=false \
-
-#     --initialize-at-run-time=org.hibernate.validator.internal.engine.messageinterpolation.el.SimpleELContext \
+#  -Dcxf.metrics.enabled=false \
+#  --initialize-at-run-time=org.hibernate.validator.internal.engine.messageinterpolation.el.SimpleELContext \
+#  -H:TraceClassInitialization=true \
+#  -Dspring.native.mode=agent
+#  -Dspring.native.remove-xml-support=false \
+#  -Dspring.spel.ignore=false \
+#  -Dspring.native.remove-yaml-support=false \
+#  --initialize-at-run-time=org.hibernate.validator.internal.engine.messageinterpolation.el.SimpleELContext \
--- src/main/java/com/cxf/server/ApplicationConfig.java
+++ src/main/java/com/cxf/server/ApplicationConfig.java
@@ -25,14 +25,33 @@ import org.apache.cxf.wsdl.ExtensionClassCreator;
 import org.apache.cxf.wsdl.ExtensionClassLoader;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.lang.annotation.Annotation;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import com.tutorialspoint.helloworld.Greetings;
+
 @Configuration
 public class ApplicationConfig {
+
+    // assumes the current class is called MyLogger
+    private final static Logger LOGGER = Logger.getLogger(ApplicationConfig.class.getName());
 
     @Autowired
     private Bus bus;
 
     @Bean
     public Endpoint endpoint(Bus bus) {
+
+        // Check package info is present
+        Package myPackage = Greetings.class.getPackage();
+        Annotation[] myPackageAnnotations = myPackage.getAnnotations();
+        System.out.println("Available annotations for package : " + myPackage.getName());
+        for(Annotation a : myPackageAnnotations) {
+            System.out.println("\t * " + a.annotationType());
+        }
+
+        LOGGER.log(Level.INFO, "ENDPOINT ENTRY");
         bus.setExtension(new WrapperHelperClassLoader(bus), WrapperHelperCreator.class);
         bus.setExtension(new ExtensionClassLoader(bus), ExtensionClassCreator.class);
         bus.setExtension(new ExceptionClassLoader(bus), ExceptionClassCreator.class);
@@ -40,9 +59,23 @@ public class ApplicationConfig {
         bus.setExtension(new FactoryClassLoader(bus), FactoryClassCreator.class);
         bus.setExtension(new GeneratedNamespaceClassLoader(bus), NamespaceClassCreator.class);
 
+        LOGGER.log(Level.INFO, "ENDPOINT Creating ednpoint object");
         EndpointImpl endpoint =
                 new EndpointImpl(bus, new HelloWorldImpl());
+        LOGGER.log(Level.INFO, "ENDPOINT Endpoint object created");
+
+
+        LOGGER.log(Level.INFO, "ENDPOINT About to publish");
         endpoint.publish("/helloworld");
+
+        LOGGER.log(Level.INFO, "ENDPOINT Published");
+        LOGGER.log(Level.INFO, "Address: " + endpoint.getAddress());
+        LOGGER.log(Level.INFO, "Bean name: " + endpoint.getBeanName());
+        LOGGER.log(Level.INFO, "Endpoint URL: " + endpoint.getPublishedEndpointUrl());
+        LOGGER.log(Level.INFO, "Binding URI: " + endpoint.getBindingUri());
+        LOGGER.log(Level.INFO, "WSDL Location: " + endpoint.getWsdlLocation());
+        LOGGER.log(Level.INFO, "Binding Config: " + endpoint.getBindingConfig().toString());
+        //LOGGER.log(Level.INFO, "Data Binding: " + endpoint.getDataBinding().toString());
 
         return endpoint;
     }

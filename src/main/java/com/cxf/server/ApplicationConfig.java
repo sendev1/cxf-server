ad3da4c75c4a6bbddc0614f556216822b2b102f1
senthil <senthilrajanr@gmail.com>
2021-02-22 09:16:37 -0600

    Updated to make CXF work with graal vm

--- src/main/java/com/cxf/server/ApplicationConfig.java
+++ src/main/java/com/cxf/server/ApplicationConfig.java
@@ -24,6 +24,7 @@ import org.apache.cxf.jaxws.spi.WrapperClassLoader;
 import org.apache.cxf.wsdl.ExtensionClassCreator;
 import org.apache.cxf.wsdl.ExtensionClassLoader;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.cxf.bus.spring.SpringBus;
 
 import java.lang.annotation.Annotation;
 import java.util.logging.Logger;
@@ -34,49 +35,58 @@ import com.tutorialspoint.helloworld.Greetings;
 @Configuration
 public class ApplicationConfig {
 
-    // assumes the current class is called MyLogger
-    private final static Logger LOGGER = Logger.getLogger(ApplicationConfig.class.getName());
+	// assumes the current class is called MyLogger
+	private final static Logger LOGGER = Logger.getLogger(ApplicationConfig.class.getName());
 
-    @Autowired
-    private Bus bus;
+	@Bean
+	public Bus cxf(){
+		final Bus bus = new SpringBus();
+		bus.setExtension(new WrapperHelperClassLoader(bus), WrapperHelperCreator.class);
+		bus.setExtension(new ExtensionClassLoader(bus), ExtensionClassCreator.class);
+		bus.setExtension(new ExceptionClassLoader(bus), ExceptionClassCreator.class);
+		bus.setExtension(new WrapperClassLoader(bus), WrapperClassCreator.class);
+		bus.setExtension(new FactoryClassLoader(bus), FactoryClassCreator.class);
+		bus.setExtension(new GeneratedNamespaceClassLoader(bus), NamespaceClassCreator.class);
+		return bus;
+	}
 
-    @Bean
-    public Endpoint endpoint(Bus bus) {
+	@Bean
+	public Endpoint endpoint(Bus bus) {
 
-        // Check package info is present
-        Package myPackage = Greetings.class.getPackage();
-        Annotation[] myPackageAnnotations = myPackage.getAnnotations();
-        System.out.println("Available annotations for package : " + myPackage.getName());
-        for(Annotation a : myPackageAnnotations) {
-            System.out.println("\t * " + a.annotationType());
-        }
+		// Check package info is present
+		Package myPackage = Greetings.class.getPackage();
+		Annotation[] myPackageAnnotations = myPackage.getAnnotations();
+		System.out.println("Available annotations for package : " + myPackage.getName());
+		for(Annotation a : myPackageAnnotations) {
+			System.out.println("\t * " + a.annotationType());
+		}
 
-        LOGGER.log(Level.INFO, "ENDPOINT ENTRY");
-        bus.setExtension(new WrapperHelperClassLoader(bus), WrapperHelperCreator.class);
+		LOGGER.log(Level.INFO, "ENDPOINT ENTRY");
+        /*bus.setExtension(new WrapperHelperClassLoader(bus), WrapperHelperCreator.class);
         bus.setExtension(new ExtensionClassLoader(bus), ExtensionClassCreator.class);
         bus.setExtension(new ExceptionClassLoader(bus), ExceptionClassCreator.class);
         bus.setExtension(new WrapperClassLoader(bus), WrapperClassCreator.class);
         bus.setExtension(new FactoryClassLoader(bus), FactoryClassCreator.class);
-        bus.setExtension(new GeneratedNamespaceClassLoader(bus), NamespaceClassCreator.class);
+        bus.setExtension(new GeneratedNamespaceClassLoader(bus), NamespaceClassCreator.class);*/
 
-        LOGGER.log(Level.INFO, "ENDPOINT Creating ednpoint object");
-        EndpointImpl endpoint =
-                new EndpointImpl(bus, new HelloWorldImpl());
-        LOGGER.log(Level.INFO, "ENDPOINT Endpoint object created");
+		LOGGER.log(Level.INFO, "ENDPOINT Creating ednpoint object");
+		EndpointImpl endpoint =
+			new EndpointImpl(bus, new HelloWorldImpl());
+		LOGGER.log(Level.INFO, "ENDPOINT Endpoint object created");
 
 
-        LOGGER.log(Level.INFO, "ENDPOINT About to publish");
-        endpoint.publish("/helloworld");
+		LOGGER.log(Level.INFO, "ENDPOINT About to publish");
+		endpoint.publish("/helloworld");
 
-        LOGGER.log(Level.INFO, "ENDPOINT Published");
-        LOGGER.log(Level.INFO, "Address: " + endpoint.getAddress());
-        LOGGER.log(Level.INFO, "Bean name: " + endpoint.getBeanName());
-        LOGGER.log(Level.INFO, "Endpoint URL: " + endpoint.getPublishedEndpointUrl());
-        LOGGER.log(Level.INFO, "Binding URI: " + endpoint.getBindingUri());
-        LOGGER.log(Level.INFO, "WSDL Location: " + endpoint.getWsdlLocation());
-        LOGGER.log(Level.INFO, "Binding Config: " + endpoint.getBindingConfig().toString());
-        //LOGGER.log(Level.INFO, "Data Binding: " + endpoint.getDataBinding().toString());
+		LOGGER.log(Level.INFO, "ENDPOINT Published");
+		LOGGER.log(Level.INFO, "Address: " + endpoint.getAddress());
+		LOGGER.log(Level.INFO, "Bean name: " + endpoint.getBeanName());
+		LOGGER.log(Level.INFO, "Endpoint URL: " + endpoint.getPublishedEndpointUrl());
+		LOGGER.log(Level.INFO, "Binding URI: " + endpoint.getBindingUri());
+		LOGGER.log(Level.INFO, "WSDL Location: " + endpoint.getWsdlLocation());
+		LOGGER.log(Level.INFO, "Binding Config: " + endpoint.getBindingConfig().toString());
+		//LOGGER.log(Level.INFO, "Data Binding: " + endpoint.getDataBinding().toString());
 
-        return endpoint;
-    }
+		return endpoint;
+	}
 }
